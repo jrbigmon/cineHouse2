985435f95992469618da8b61a05a37a44a7a4d54
jrbigmon <junioterriaga0@gmail.com>
2022-04-03 13:31:17 -0300

    Adicionado listarFilmesCartaz() para teste

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const alterarStatus = require("./metodos/alterarStatus");
 const catalogo = require("./catalogo/catalogo.json");
+const alterarStatus = require("./metodos/alterarStatus");
+const listarFilmesCartaz = require("./metodos/listarFilmesCartaz");
 
-alterarStatus(catalogo, 1)
+console.table(alterarStatus(catalogo, 1))
+console.table(listarFilmesCartaz(catalogo, true))
