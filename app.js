3780390441222ae7b7b059a936fe3bbfd8f9bd20
jrbigmon <junioterriaga0@gmail.com>
2022-04-03 14:03:12 -0300

    Testando adicionarFilme.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const alterarStatus = require("./metodos/alterarStatus");
 const listarFilmesCartaz = require("./metodos/listarFilmesCartaz");
 const listarFilmesLongaDuracao = require("./metodos/listarFilmesLongaDuracao");
 const listarTodosOsFilmes = require("./metodos/listarTodosOsFilmes");
-
+const adicionarFilme = require("./metodos/adicionarFilme")
 // console.table(alterarStatus(catalogo, 1))
 // console.table(listarFilmesCartaz(catalogo, true))
 // console.table(listarTodosOsFilmes(catalogo))
-console.table(listarFilmesLongaDuracao(catalogo))
+// console.table(listarFilmesLongaDuracao(catalogo))
+console.table(adicionarFilme(catalogo, "Rambo", "Sivestre Estalando", 2004, 200, true))
