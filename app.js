5a2d25cb0291abfa398250101fbfae4f03304d99
jrbigmon <junioterriaga0@gmail.com>
2022-04-03 13:41:48 -0300

    testando listarTodosOsFilmes.js

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const catalogo = require("./catalogo/catalogo.json");
 const alterarStatus = require("./metodos/alterarStatus");
 const listarFilmesCartaz = require("./metodos/listarFilmesCartaz");
+const listarTodosOsFilmes = require("./metodos/listarTodosOsFilmes");
 
 console.table(alterarStatus(catalogo, 1))
 console.table(listarFilmesCartaz(catalogo, true))
+console.table(listarTodosOsFilmes(catalogo))
