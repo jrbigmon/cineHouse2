f1823ddafd45e6e473ccb4b53feb9e848fef49d6
jrbigmon <junioterriaga0@gmail.com>
2022-04-03 13:51:26 -0300

    Testando listarFilmesLongaDuracao.js

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const catalogo = require("./catalogo/catalogo.json");
 const alterarStatus = require("./metodos/alterarStatus");
 const listarFilmesCartaz = require("./metodos/listarFilmesCartaz");
+const listarFilmesLongaDuracao = require("./metodos/listarFilmesLongaDuracao");
 const listarTodosOsFilmes = require("./metodos/listarTodosOsFilmes");
 
-console.table(alterarStatus(catalogo, 1))
-console.table(listarFilmesCartaz(catalogo, true))
-console.table(listarTodosOsFilmes(catalogo))
+// console.table(alterarStatus(catalogo, 1))
+// console.table(listarFilmesCartaz(catalogo, true))
+// console.table(listarTodosOsFilmes(catalogo))
+console.table(listarFilmesLongaDuracao(catalogo))
