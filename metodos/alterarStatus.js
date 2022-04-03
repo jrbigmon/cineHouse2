410730a87246478dd7f181a2e785b89533f3b011
jrbigmon <junioterriaga0@gmail.com>
2022-04-03 13:30:32 -0300

    Alterado de console.table para return na funcao alterarStatus

--- metodos/alterarStatus.js
+++ metodos/alterarStatus.js
@@ -2,9 +2,7 @@ const buscarFilme = require("./buscarFilme");
 
 function alterarStatus(lista, codigo){
     let sit = buscarFilme(lista, codigo)
-    console.table(sit)
-    console.log('-'.repeat(30))
     sit.emCartaz ? sit.emCartaz = false : sit.emCartaz = true
-    console.table(sit)
+    return sit
 }
 module.exports = alterarStatus;
